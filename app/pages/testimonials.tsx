56758b9a3fc5427105d2a04a9d94697c313bf10d
Undead5z <alikumuhammedfaizan@gmail.com>
2024-05-28 01:09:00 +0530

    updated testimonials

--- app/pages/testimonials.tsx
+++ app/pages/testimonials.tsx
@@ -19,9 +19,9 @@ export function TestimonialsSection() {
 const testimonials = [
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+      "Best App buy it!!!.",
+    name: "someguy",
+    title: "CEO of somecompany",
   },
   {
     quote:
